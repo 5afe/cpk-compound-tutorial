4a9169cc1b1441758c4389009180798f0634ab88
Mikhail Mikheev <mmvsha73@gmail.com>
2020-01-16 17:02:48 +0400

    Add averta font, styling WIP

--- src/App.tsx
+++ src/App.tsx
@@ -1,9 +1,11 @@
 import React from 'react';
+import GlobalStyles from 'src/styles/global'
 import ConnectButton from 'src/components/ConnectButton'
 
 const App: React.FC = () => {
   return (
     <div className="App">
+      <GlobalStyles />
       Safe Contract Proxy Kit Example
       <ConnectButton />
     </div>
--- src/styles/global.ts
+++ src/styles/global.ts
@@ -1,11 +1,32 @@
-import { createGlobalStyle } from 'styled-components'
-import { normalize } from 'styled-normalize'
+import { createGlobalStyle } from "styled-components"
+import { normalize } from "styled-normalize"
+import Averta from 'src/assets/fonts/Averta-normal.woff2'
+import AvertaBold from 'src/assets/fonts/Averta-ExtraBold.woff2'
 
 const GlobalStyles = createGlobalStyle`
   ${normalize}
 
+
+  @font-face {
+    font-family: "Averta";
+    font-style: normal;
+    font-weight: 400;
+    font-display: swap;
+    src: local("Averta-Regular"), 
+      url(${Averta}) format("woff2");
+  }
+
+  @font-face {
+    font-family: "Averta";
+    font-style: normal;
+    font-weight: 800;
+    font-display: swap;
+    src: local("Averta-Extrabold"),
+      url(${AvertaBold}) format("woff2");
+  }
+
   body {
-    font-family: 'Karla', sans-serif;
+    font-family: 'Averta', sans-serif;
     margin: 0;
     padding: 0;
   }
